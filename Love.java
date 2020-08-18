35f5270e659de0edc458f23831327dd236fef50d
jpolacco <jpolacco@gmail.com>
2020-08-17 22:57:56 -0700

    Add files via upload

---
+++ Love.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class Love here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Love
+{
+    
+    /**
+     * Takes a String and replaces the first occurrence of "hate" with "love". 
+     * If the word "hate" does not appear in the String, this method returns
+     * the original String.
+     * If the word "hate" occurs more than once in the String, 
+     * this method replaces only the first occurrence of "hate".
+     * 
+     * Note: you can only use the indexOf and substring methods. You will not get
+     * credit if you use any of the other String methods such as replace()
+     */
+    public String loveNotHate(String str){
+        // TODO
+        return ""; // delete this line of code
+    }
+}
